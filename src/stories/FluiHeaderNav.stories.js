dc2d60cb9c9672fef5a9b318079a397a918f6bb1
julianaa07 <juliana.garcia@lkmx.io>
2021-10-27 10:40:15 -0700

    Added FluiHeaderNav component
    - Added FluiHeaderNav.vue
    - Created FluiHeaderNav.stories.js

---
+++ src/stories/FluiHeaderNav.stories.js
@@ -0,0 +1,30 @@
+import FluiHeaderNav from './FluiHeaderNav.vue';
+import { withDesign } from 'storybook-addon-designs'
+
+export default {
+  title: 'FlareUi/HeaderNav',
+  component: FluiHeaderNav,
+  decorators: [withDesign],
+  argTypes: {
+    title: '',
+    message: '',
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { FluiHeaderNav },
+  template: '<flui-header-nav v-bind="$props" />',
+});
+
+export const Normal = Template.bind({});
+Normal.args = {
+  title: 'Software engineering 👩🏻‍💻',
+  message: 'Software engineering is the systematic application of engineering approaches to the development of software. A software engineer is a person who applies the principles of software engineering to design, develop, maintain, test, and evaluate computer software.',
+};
+Normal.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/Zv5cp2PFUMFR3G3fp6qb15/Flare-Igniter-🧬?node-id=862%3A1436',
+  },
+}
